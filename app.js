a21f33190f2c9f8cd7834ce6b90067535a2466c4
efazri <egyfazri23@gmail.com>
2020-10-23 11:06:05 +0700

    redeploy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require ('express')
 const app = express ()
 const router = require ('./routes')
 const cors = require ('cors')
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 
 app.use(express.urlencoded({extended:false}))
